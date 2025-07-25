e928fa4a89bd00e5c1b7df11fceb22161c49c867
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-07-25 22:23:50 +0530

    DB adding

---
+++ src/main/java/util/DBConnection.java
@@ -0,0 +1,13 @@
+package util;
+
+import java.sql.*;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/attendance_db";
+    private static final String USER = "root";
+    private static final String PASS = "your_password";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
