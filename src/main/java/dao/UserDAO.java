14e1d9325a231ce63a7fb01c6bdedd110548234e
Lakidu Minosha Senarathne <93633069+lakindu1234@users.noreply.github.com>
2025-07-25 22:25:34 +0530

    chnges  in files

---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,29 @@
+package dao;
+
+import model.User;
+import util.DBConnection;
+import java.sql.*;
+
+public class UserDAO {
+    public User findByEmail(String email) {
+        try (Connection conn = DBConnection.getConnection()) {
+            String query = "SELECT * FROM users WHERE email = ?";
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, email);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setEmail(rs.getString("email"));
+                user.setPassword(rs.getString("password"));
+                user.setRole(rs.getString("role"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
